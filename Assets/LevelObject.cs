97c4490cd86d9a1d7f32f52bc65b6e40d92fea89
DarthSethious <sjaugmented@gmail.com>
2020-05-12 09:04:41 -0700

    turned on spatial awareness

--- Assets/LevelObject.cs
+++ Assets/LevelObject.cs
@@ -13,6 +13,7 @@ public class LevelObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y - 1, Camera.main.transform.position.z);
+        transform.rotation = new Quaternion(0, Camera.main.transform.rotation.y, 0, Camera.main.transform.rotation.w);
     }
 }
