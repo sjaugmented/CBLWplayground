4c2a97018667c1e2a5842422ffaba6e39be57781
DarthSethious <sjaugmented@gmail.com>
2020-05-15 09:01:01 -0700

    added rotation to master orb

---
+++ Assets/MasterOrbRotater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterOrbRotater : MonoBehaviour
+{
+    [SerializeField] float xRotation = 1f;
+    [SerializeField] float yRotation = 1f;
+    [SerializeField] float zRotation = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xRotation, yRotation, zRotation);
+    }
+}
