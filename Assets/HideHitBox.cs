8dcb1d47fd81ae70dec2a47d55553318c1048b65
DarthSethious <sjaugmented@gmail.com>
2020-06-05 18:32:44 -0700

    + skypanel gaze box

---
+++ Assets/HideHitBox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideHitBox : MonoBehaviour
+{
+    Renderer thisRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRenderer = GetComponent<Renderer>();
+    }
+
+    public void HideBox()
+    {
+        thisRenderer.enabled = !thisRenderer.enabled;
+    }
+}
