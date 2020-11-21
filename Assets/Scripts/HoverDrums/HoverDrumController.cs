5cde957e28bf445f4e916ec66b65a46815a8bca6
DarthSethious <sjaugmented@gmail.com>
2020-11-21 11:41:57 -0800

    basic touch and handling added to drum object

--- Assets/Scripts/HoverDrums/HoverDrumController.cs
+++ Assets/Scripts/HoverDrums/HoverDrumController.cs
@@ -6,15 +6,33 @@ namespace LW.HoverDrums
 {
     public class HoverDrumController : MonoBehaviour
     {
-        [SerializeField] Color color;
+        [SerializeField] [Range(0, 1)] float hFloat = 0f;
+        [SerializeField] string oscMessage = "shape";
+
+        OSC osc;
+
         void Start()
         {
-        
+            osc = GameObject.FindGameObjectWithTag("OSC").GetComponent<OSC>();
         }
 
         void Update()
         {
-            GetComponentInChildren<Renderer>().material.color = color;
+            GetComponentInChildren<Renderer>().material.color = Color.HSVToRGB(hFloat, 1, 1);
+        }
+
+        public void Touched()
+        {
+            SendOSCMessage(oscMessage, hFloat);
+        }
+
+        private void SendOSCMessage(string address, float value)
+        {
+            OscMessage message = new OscMessage();
+            message.address = address;
+            message.values.Add(value);
+            osc.Send(message);
+            Debug.Log(this.gameObject.name + " sending OSC:" + message); // todo remove
         }
     }
 
