568707764c17271f03ecb319a4a9bcc0142785e5
DarthSethious <sjaugmented@gmail.com>
2020-12-11 14:51:49 -0800

    +RuneBelt and RuneTypes

---
+++ Assets/Scripts/Runes/RuneBelt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LW.Runic
+{
+    public class RuneBelt : MonoBehaviour
+    {
+        //[SerializeField] List<GameObject> runePrefabs;
+        [SerializeField] RuneSlot[] runeSlots;
+
+		public int GetRuneSlots()
+		{
+			return runeSlots.Length;
+		}
+
+        [System.Serializable]
+        private class RuneSlot
+		{
+			[SerializeField] GameObject prefab;
+			public RuneType runeType;
+            public int runeCount = 8;
+		}
+
+		//public int GetCurrentRuneCount()
+		//{
+		//	return runeCount;
+		//}
+
+		//public void ReduceCurrentRuneCount()
+		//{
+		//	runeCount--;
+		//}
+	}
+}
