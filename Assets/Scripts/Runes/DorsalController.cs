3818939cec432bb3267db711a23cc25fc27168e3
DarthSethious <sjaugmented@gmail.com>
2020-12-12 15:51:07 -0800

    build/touch indicators

---
+++ Assets/Scripts/Runes/DorsalController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LW.Runic
+{
+    public class DorsalController : MonoBehaviour
+    {
+        [SerializeField] Material buildActiveColor;
+        [SerializeField] Material buildInactiveColor;
+        
+        List<Renderer> childRend = new List<Renderer>();
+
+        RunicDirector director;
+        void Start()
+        {
+            director = GameObject.FindGameObjectWithTag("Director").GetComponent<RunicDirector>();
+
+            foreach(Transform t in transform)
+			{
+                childRend.Add(t.GetComponent<Renderer>());
+			}
+        }
+
+        void Update()
+        {
+            if (director.currentMode == RunicDirector.Mode.Build)
+			{
+                SetDorsalMaterial(buildActiveColor);
+			}
+			else
+			{
+                SetDorsalMaterial(buildInactiveColor);
+            }
+        }
+
+		private void SetDorsalMaterial(Material mat)
+		{
+			foreach (Renderer color in childRend)
+			{
+				color.material = mat;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Runes/FingerParticleController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+namespace LW.Runic
+{
+    public class FingerParticleController : MonoBehaviour
+    {
+        RunicDirector director;
+        EmissionModule emission;
+        void Start()
+        {
+            director = GameObject.FindGameObjectWithTag("Director").GetComponent<RunicDirector>();
+            emission = GetComponentInChildren<ParticleSystem>().emission;
+        }
+
+        void Update()
+        {
+			if (director.currentMode == RunicDirector.Mode.Touch) emission.enabled = true;
+			else emission.enabled = false;
+		}
+    }
+
+}
