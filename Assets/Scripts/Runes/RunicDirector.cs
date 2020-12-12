e91ddd64bf1ab9063da16a9ddecc41b0b3176fc0
DarthSethious <sjaugmented@gmail.com>
2020-12-12 13:18:03 -0800

    +Director, workshopping OnTriggerEnter

---
+++ Assets/Scripts/Runes/RunicDirector.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LW.Core;
+
+namespace LW.Runic
+{
+    public class RunicDirector : MonoBehaviour
+    {
+        [SerializeField] GameObject rightPointer, leftPointer, rightToggle, leftToggle, rightDorsal, leftDorsal;
+
+        List<GameObject> rightHand = new List<GameObject>();
+        List<GameObject> leftHand = new List<GameObject>();
+
+		HandTracking handtracking;
+        
+        void Start()
+		{
+			handtracking = GameObject.FindGameObjectWithTag("Handtracking").GetComponent<HandTracking>();
+			
+			rightHand.Add(rightPointer);
+			rightHand.Add(rightToggle);
+			rightHand.Add(rightDorsal);
+			leftHand.Add(leftPointer);
+			leftHand.Add(leftToggle);
+			leftHand.Add(leftDorsal);
+
+			SetRightHand(false);
+			SetLeftHand(false);
+		}
+
+		private void SetRightHand(bool set)
+		{
+			foreach (GameObject asset in rightHand)
+			{
+				asset.SetActive(set);
+			}
+		}
+
+		private void SetLeftHand(bool set)
+		{
+			foreach (GameObject asset in leftHand)
+			{
+				asset.SetActive(set);
+			}
+		}
+
+		void Update()
+        {
+			if (handtracking.rightHand) SetRightHand(true);
+			else SetRightHand(false);
+
+			if (handtracking.leftHand) SetLeftHand(true);
+			else SetLeftHand(false);
+        }
+
+        public void ToggleMode()
+		{
+            Debug.Log("Director: toggled");
+		}
+    }
+}
---
+++ Assets/Scripts/Runes/RunicHandToggle.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LW.Runic
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(BoxCollider))]
+    public class RunicHandToggle : MonoBehaviour
+    {
+        [SerializeField] bool leftHand = false;
+        bool triggered = false;
+
+        RunicDirector director;
+
+        void Start()
+        {
+            director = GameObject.FindGameObjectWithTag("Director").GetComponent<RunicDirector>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+		{
+            Debug.Log("collision");
+            if (leftHand)
+			{
+                Debug.Log("left hand");
+                if (other.CompareTag("Right Pointer"))
+                {
+                    Debug.Log("rightPointer");
+                    if (!triggered)
+                    {
+                        Debug.Log("HandToggle: toggled");
+                        director.ToggleMode();
+                        triggered = true;
+                        StartCoroutine("ToggleDelay");
+                    }
+                }
+            }
+
+            else
+			{
+                Debug.Log("right hand");
+                if (other.CompareTag("Left Pointer"))
+                {
+                    Debug.Log("leftPointer");
+                    if (!triggered)
+                    {
+                        Debug.Log("HandToggle: toggled");
+                        director.ToggleMode();
+                        triggered = true;
+                        StartCoroutine("ToggleDelay");
+                    }
+
+                }
+            }            
+            
+        }
+
+        IEnumerator ToggleDelay()
+        {
+            yield return new WaitForSeconds(0.2f);
+            triggered = !triggered;
+        }
+    }
+
+}
