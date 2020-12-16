946589c4372239a93a9c53f6d8961b29b556fb1d
DarthSethious <sjaugmented@gmail.com>
2020-12-16 15:33:27 -0800

    added RuneTouch inside manipulation collider

---
+++ Assets/Scripts/Runes/RuneTouch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LW.Core;
+
+namespace LW.Runic
+{
+    public class RuneTouch : MonoBehaviour
+    {
+        [SerializeField] float touchDistance = 0.03f;
+
+        public float distance = Mathf.Infinity;
+        
+        Transform rightPointer;
+        bool touched = false;
+
+        void Start()
+		{
+            //rightPointer = GameObject.FindGameObjectWithTag("Right Pointer").transform;
+		}
+
+        void Update()
+		{
+            if (!GameObject.FindGameObjectWithTag("Right Pointer")) return;
+
+            distance = Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Right Pointer").transform.position);
+
+            if (distance < touchDistance)
+			{
+                if (!touched)
+				{
+                    GetComponentInParent<RuneController>().Touched();
+                    touched = true;
+				}
+			}
+            else
+			{
+                GetComponentInParent<RuneController>().NotTouched();
+                touched = false;
+			}
+		}
+    }
+}
