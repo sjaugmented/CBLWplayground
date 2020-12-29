39d449156fa7d309e8b9fe509100c1b0884ad2c2
DarthSethious <sjaugmented@gmail.com>
2020-12-29 12:17:26 -0800

    +first node to base prefab +NodeController

---
+++ Assets/Scripts/Runes/NodeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LW.Runic
+{
+    public class NodeController : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+        public void Touched()
+        {
+            Debug.Log("touched");
+        }
+    }
+}
