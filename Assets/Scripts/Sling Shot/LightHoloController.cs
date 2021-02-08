3ac35f71da7f2a74b98f32a3647dcc3bea95aca9
DarthSethious <sjaugmented@gmail.com>
2021-02-07 16:11:03 -0800

    initial throw/lasso functionality to test

--- Assets/Scripts/Sling Shot/LightHoloController.cs
+++ Assets/Scripts/Sling Shot/LightHoloController.cs
@@ -7,8 +7,15 @@ namespace LW.SlingShot
 {
 	public class LightHoloController : MonoBehaviour
 	{
-		private HandTracking hands;
-		private bool lassoPrimed;
+		[SerializeField] float maxHandDist = 0.5f;
+		[SerializeField] bool devMode = false;
+		[Range(10, 200)] float force;
+		
+		HandTracking hands;
+		public bool holoOut = false;
+		public bool lassoPrimed = false;
+		public bool recall = false;
+		//private Vector3 currentPos;
 
 		void Start()
 		{
@@ -17,7 +24,9 @@ namespace LW.SlingShot
 
 	    void Update()
 	    {
-		    if (hands.palmsOut && hands.rightOpen && hands.leftOpen)
+		    //currentPos = transform.position;
+		    
+		    if (hands.palmsOut && hands.rightOpen && hands.leftOpen && !holoOut)
 		    {
 			    ThrowHolo();
 		    }
@@ -25,30 +34,71 @@ namespace LW.SlingShot
 		    if (hands.palmsIn && !hands.rightFist && !hands.leftFist)
 		    {
 			    lassoPrimed = true;
-
-			    if (hands.rightFist && hands.leftFist)
-			    {
-				    LassoHolo();
-			    }
 		    }
 		    else
 		    {
 			    lassoPrimed = false;
 		    }
-	    }
+
+		    if (lassoPrimed && hands.rightFist && hands.leftFist)
+		    {
+			    recall = true;
+		    }
+
+		    if (recall && holoOut)
+		    {
+			    Vector3 lassoOrigin = Camera.main.transform.position - new Vector3(0, 0.3f, 0);
+			    transform.LookAt(lassoOrigin);
+			    if (transform.position != lassoOrigin)
+			    {
+					GetComponent<Rigidbody>().AddForce((transform.forward * 5));
+			    }
+			    else
+			    {
+				    recall = false;
+				    Component[] childRends = GetComponentsInChildren<Renderer>();
+				    foreach (Renderer rend in childRends)
+				    {
+					    rend.enabled = false;
+				    }
+			    }
+		    }
+
+		    #region DEV CONTROLS
+		    if (devMode)
+		    {
+			    force += Input.mouseScrollDelta.y;
+			    if (Input.GetKeyDown(KeyCode.Z))
+			    {
+					ThrowHolo();
+			    }
+
+			    if (Input.GetKeyDown(KeyCode.G))
+			    {
+				    recall = true;
+			    }
+
+		    }
+		    
+
+		    #endregion
+		}
 
 	    private void ThrowHolo()
 	    {
-		    Vector3 castOrigin = Vector3.Lerp(hands.rightPalm.Position, hands.leftPalm.Position, 0.5f);
+			Component[] childRends = GetComponentsInChildren<Renderer>();
+			foreach (Renderer rend in childRends)
+			{
+				rend.enabled = true;
+			}
+
+			Vector3 castOrigin = Vector3.Lerp(hands.rightPalm.Position, hands.leftPalm.Position, 0.5f);
 		    Quaternion castRotation = Quaternion.Slerp(hands.rightPalm.Rotation, hands.leftPalm.Rotation, 0.5f) *
 		                              Quaternion.Euler(60, 0, 0);
 
+			GetComponent<Rigidbody>().AddForce(transform.forward * force);
 
-	    }
-
-	    private void LassoHolo()
-	    {
-		    throw new System.NotImplementedException();
+			holoOut = true;
 	    }
 	}
 	
